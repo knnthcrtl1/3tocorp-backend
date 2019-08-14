a9666a5c2847d4c0ee20f83fd5d687272472da2d
unknown <kcartel@gofluent.com>
2019-08-14 11:25:12 +0800

    student pagination

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -4,13 +4,35 @@ const bcrypt = require('bcryptjs');
 const myPlaintextPassword = 'test123';
 
 exports.student_get = async (req, res) => {
-    const student = await studentModel.find({});
+    // const student = await studentModel.find({});
 
-    try {
-        res.send(student);
-    } catch (err) {
-        res.status(500).send(err);
+    let pageNo = parseInt(req.query.pageNo)
+    let size = parseInt(req.query.size)
+
+    let query = {}
+    if(pageNo < 0 || pageNo === 0) {
+        response = {"error" : true,"message" : "invalid page number, should start with 1"};
+        return res.json(response)
     }
+    query.skip = size * (pageNo - 1);
+    query.limit = size;
+  // Find some documents
+  studentModel.countDocuments({},(err, totalCount) => {
+     if(err) {
+       response = {"error" : true,"message" : "Error fetching data"}
+   }
+   
+   studentModel.find({},{},query,(err,data) => {
+              // Mongo command to fetch all data from collection.
+              if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                var totalPages = Math.ceil(totalCount / size)
+                response = {"error" : false,"message" : data,"pages": totalPages};
+            }
+            res.json(response);
+        });
+})
 
 }
 
