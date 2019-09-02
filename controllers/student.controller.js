19e9127b1fc2db6f4a6d9645746fa6861a6d2ea6
unknown <kcartel@gofluent.com>
2019-09-02 15:56:01 +0800

    add jwt token

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -54,28 +54,29 @@ exports.student_get = async (req, res) => {
 
 exports.student_create = async (req, res) => {
 
-    const student = new studentModel(req.body);
+    // const student = new studentModel(req.body);
 
     try {
 
-        await student.save();
+        console.log(await req.body);
+        // await student.save();
 
-        res.send(student._id);
+        // res.send(student._id);
 
 
-        bcrypt.genSalt(10, async (err, salt) => {
-            bcrypt.hash(myPlaintextPassword, salt, async (err, hash) => {
-                // Store hash in your password DB.
-                const user = new userModel({
-                    user_id: student._id,
-                    username: student.email,
-                    password: hash,
-                    restriction: 1
-                });
+        // bcrypt.genSalt(10, async (err, salt) => {
+        //     bcrypt.hash(myPlaintextPassword, salt, async (err, hash) => {
+        //         // Store hash in your password DB.
+        //         const user = new userModel({
+        //             user_id: student._id,
+        //             username: student.email,
+        //             password: hash,
+        //             restriction: 1
+        //         });
 
-                await user.save();
-            });
-        });
+        //         await user.save();
+        //     });
+        // });
 
 
     } catch (err) {
